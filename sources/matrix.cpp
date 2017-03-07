ebef1f327efc2b8a850221f6b5f75c44710b9342
rtv22 <rtvcojigor@gmail.com>
2017-03-07 13:31:23 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,15 +1,76 @@
 #include "matrix.hpp"
 
-matrix_t::matrix_t() noexcept : rows_{0}, columns_{0}, elements_{nullptr}
-{
+Matrix::Matrix(){
+	n = 0;
+	m = 0;
 }
 
-auto matrix_t::rows() -> unsigned int
-{
-    return rows_;
+Matrix::Matrix(int xSize, int ySize){ //заполнить матрицу 0
+	p = new int * [ySize];
+	for (int count = 0; count < ySize; count++)
+		p[count] = new int[xSize];
+	n = xSize;
+	m = ySize;
 }
 
-auto matrix_t::columns() -> unsigned int
-{
-    return columns_;
+void Matrix::fill(){
+	ifstream file("file.txt");
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < m; j++){
+			file >> p[i][j];
+		}
+	}
+}
+
+void Matrix::printMatrix() {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << p[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << flush;
+}
+
+Matrix::Matrix(Matrix&MatrixCopy){
+	n = MatrixCopy.n;
+	m = MatrixCopy.m;
+	p = new int*[n];
+	for (int i = 0; i < n; i++) {
+		p[i] = new int[m];
+		for (int j = 0; j < m; j++) {
+			p[i][j] = MatrixCopy.p[i][j];
+		}
+	}
+}
+
+Matrix Matrix::operator+(Matrix MatrixCopy)const {
+
+	Matrix re(n, m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			re.p[i][j] = p[i][j] + MatrixCopy.p[i][j];
+		}
+	}
+	return re;
+}
+
+Matrix Matrix::operator*(Matrix MatrixCopy)const {
+	Matrix re(n, m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			re.p[i][j] = 0;
+			for (int k = 0; k < m; k++) 
+				re.p[i][j] += p[i][k] * MatrixCopy.p[k][j];
+		}
+	}
+	return re;
+}
+
+Matrix::~Matrix(){
+	for (int i = 0; i < n; i++){
+		delete[] p[i];
+	}
+	delete[] p; 
 }
