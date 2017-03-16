b8c9ecea2d67f7c0be154966d9504be45eb5f4f8
rtv22 <rtvcojigor@gmail.com>
2017-03-16 11:38:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,20 +6,3 @@ SCENARIO("matrix init", "[init]") {
 	REQUIRE(matrix.rows() == 0);
 	REQUIRE(matrix.columns() == 0);
 }
-
-SCENARIO("with params", "[init]")
-{
-	int init = 5;
-	Matrix temp(init, init);
-	REQUIRE(temp.rows() == 5);
-	REQUIRE(temp.columns() == 5);
-}
-
-SCENARIO("copy", "[init]")
-{
-	int init = 5;
-	Matrix temp(init, init);
-	Matrix copy(temp);
-	REQUIRE(copy.rows() == 5);
-	REQUIRE(copy.columns() == 5);
-}
