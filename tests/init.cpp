a2e17f3e92f6d049369d7bb61bb8c10ced3e9994
rtv22 <rtvcojigor@gmail.com>
2017-03-16 11:23:29 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,25 @@
-#include <matrix.hpp>
+#include <classmatrix.hpp>
 #include <catch.hpp>
 
 SCENARIO("matrix init", "[init]") {
 	matrix_t matrix;
 	REQUIRE(Matrix_1.xSize() == 0);
 	REQUIRE(Matrix_1.ySize() == 0);
+}
+
+SCENARIO("with params", "[init]")
+{
+	int init = 5;
+	Matrix temp(init, init);
+	REQUIRE(temp.rows() == 5);
+	REQUIRE(temp.columns() == 5);
+}
+
+SCENARIO("copy", "[init]")
+{
+	int init = 5;
+	Matrix temp(init, init);
+	Matrix copy(temp);
+	REQUIRE(copy.rows() == 5);
+	REQUIRE(copy.columns() == 5);
 }
