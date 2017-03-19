b72fa40951984f651b6c6df7e819d868b3d1fc50
rtv22 <rtvcojigor@gmail.com>
2017-03-20 01:04:23 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -7,13 +7,15 @@ public:
 	Matrix(); // (1)
 	Matrix(int, int);// (2)
 	~Matrix();// (3)	
-	void fill(string);// (4)
+	void fill();// (4)
 	void printMatrix(); //(5)
 	Matrix operator+(Matrix)const;// (6)
 	Matrix operator*(Matrix)const;// (7)
 	Matrix(Matrix &MatrixCopy);
+	auto rows() -> unsigned int;
+    	auto columns() -> unsigned int;
 	bool operator==(Matrix&)const;
 	Matrix operator=(Matrix &MatrixCopy);
-	friend ostream& operator<<(ostream&, Matrix &MatrixCopy);
-	friend istream& operator>>(istream&, Matrix &MatrixCopy);
+	friend ostream& operator<<(ostream& rez, Matrix &MatrixCopy);
+	friend istream& operator>>(istream& rez, Matrix &MatrixCopy);
 };
