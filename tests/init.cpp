47b9d159bde76835e9108dc0706c07728be5bfdd
rtv22 <rtvcojigor@gmail.com>
2017-03-19 15:33:46 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -32,7 +32,7 @@ SCENARIO("ADD", "[oper+]")
 	std::ifstream ("Matr_1.txt") >> a;
 	std::ifstream ("Matr_2.txt") >> b;
 	std::ifstream("rez.txt") >> rez;
-	REQUIRE(a+b == rez);
+	REQUIRE((a+b) == rez);
 }
 
 SCENARIO("mult", "[oper*]")
@@ -43,5 +43,5 @@ SCENARIO("mult", "[oper*]")
 	std::ifstream("Matr_1.txt") >> a;
 	std::ifstream("Matr_2.txt") >> b;
 	std::ifstream("rez_1.txt") >> rez;
-	REQUIRE(a * b == rez);
+	REQUIRE((a * b) == rez);
 }
