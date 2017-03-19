00eb25edb7d3ac179cc0c30ee3e3b82fadeab00b
rtv22 <rtvcojigor@gmail.com>
2017-03-19 22:51:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -26,23 +26,15 @@ SCENARIO("copy", "[init]")
 	REQUIRE(copy.columns() == 2);
 }
 
-SCENARIO("comp", "[init]")
-{
-	Matrix a(2, 2);
-	Matrix b(2, 2);
-	std::ifstream ("Matr_1.txt") >> a;
-	std::ifstream ("Matr_1_1.txt") >> b;
-	REQUIRE(a == b);
-}
 SCENARIO("add", "[init]")
 {
-	Matrix a(2, 2);
-	Matrix b(2, 2);
-	Matrix c(2, 2);
-	std::ifstream ("Matr_1.txt") >> a;
-	std::ifstream ("Matr_2.txt") >> b;
-	std::ifstream ("rez.txt") >> c;
-	REQUIRE((a + b) == c);
+	Matrix A(2, 2);
+	Matrix B(2, 2);
+	Matrix C(2, 2);
+	std::ifstream ("Matr_1.txt") >> A;
+	std::ifstream ("Matr_2.txt") >> B;
+	std::ifstream ("rez.txt") >> C;
+	REQUIRE((A + B) == C);
 }
 
 
