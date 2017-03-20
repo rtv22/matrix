b9e2cff6dcf78c11047fba2d1f839047a5edd0b9
rtv22 <rtvcojigor@gmail.com>
2017-03-20 09:52:56 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -10,14 +10,14 @@ SCENARIO("matrix init", "[init]") {
 	REQUIRE(matrix.columns() == 0);
 }
 
-SCENARIO("params init", "[init]") {
+SCENARIO("params init", "[init with params]") {
 	int init = 5;
 	Matrix matrix(init, init);
 	REQUIRE(matrix.rows() == 5);
 	REQUIRE(matrix.columns() == 5);
 }
 
-SCENARIO("copy", "[init]")
+SCENARIO("copy", "[copy]")
 {
 	int init = 2;
 	Matrix temp(init, init);
@@ -26,7 +26,7 @@ SCENARIO("copy", "[init]")
 	REQUIRE(copy.columns() == 2);
 }
 
-SCENARIO("add", "[init]")
+SCENARIO("add", "[oper+]")
 {
 	Matrix A(2, 2);
 	Matrix B(2, 2);
@@ -37,4 +37,25 @@ SCENARIO("add", "[init]")
 	REQUIRE((A + B) == C);
 }
 
+SCENARIO("comp", "[oper=]")
+{
+	Matrix A(2, 2);
+	Matrix B(2, 2);
+	std::ifstream("Matr_1.txt") >> A
+	A = B;
+	REQUARE(A == B);
+}
+
+SCENARIO("mult", "[mult]")
+{
+	Matrix A (2, 2);
+	Matrix B (2, 2);
+	Matrix C (2, 2);
+	std::ifstream("Matr_1.txt") >> A;
+	std::ifstream("Matr_2.txt") >> B;
+	std::ifstream("rez_1.txt") >> C;
+	REQUARE((A*B) == C);
+}
+		      
+SCENARIO("")		      
 
